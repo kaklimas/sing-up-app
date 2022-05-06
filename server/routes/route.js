ae5e0c0505c62845a046973a033105f042864efc
kaklimas <klimaskacper@interia.pl>
2022-05-06 15:38:01 +0200

    backend things've done

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const signUpTemplateCopy = require('../models/SignUpModels.js')
+const bcrypt = require('bcrypt')
 
+router.post('/signup', async (req, res) => {
+    
+    const saltPassword = await bcrypt.genSalt(10)
+    const securePassword = await bcrypt.hash(req.body.password, saltPassword)
 
-router.post('/signup', (req, res) => {
     const signedUser = new signUpTemplateCopy({
-
+        fullName:req.body.fullName,
+        username: req.body.username,
+        email: req.body.email,
+        password: securePassword,
     })
+    signedUser.save()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(error => res.json(error))
 })
 
 module.exports = router
